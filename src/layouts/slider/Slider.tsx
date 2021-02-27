fb44c221e67956e32d36b4a3b9f112acfad89128
souravlayek <souravlayek11@gmail.com>
2021-02-27 16:44:45 +0530

    slider scroll

--- src/layouts/slider/Slider.tsx
+++ src/layouts/slider/Slider.tsx
@@ -1,25 +1,41 @@
-import React from 'react'
-import SliderItem from '../../components/slider/SliderItem'
-import './css/slider.css'
-import {MdNavigateNext} from "react-icons/md"
-
-
-interface Props {
-
-}
+import React, { useRef } from "react";
+import SliderItem from "../../components/slider/SliderItem";
+import "./css/slider.css";
+import { MdNavigateNext } from "react-icons/md";
+import "react-alice-carousel/lib/alice-carousel.css";
+import AliceCarousel from "react-alice-carousel";
+interface Props {}
 
 const Slider = (props: Props) => {
+  const slider = useRef<any>(null);
+
+  const items = [
+    <SliderItem />,
+    <SliderItem />,
+    <SliderItem />,
+    <SliderItem />,
+  ];
   return (
     <div className="slider">
-      <div className="button left">
+      <div onClick={() => slider.current.slidePrev()} className="button left">
         <MdNavigateNext className="leftarrow" size="40px" />
       </div>
-      <div className="button right">
+      <div onClick={() => slider.current.slideNext()} className="button right">
         <MdNavigateNext className="rightarrow" size="40px" />
       </div>
-      <SliderItem />
+      <AliceCarousel
+        animationDuration={500}
+        ref={slider}
+        infinite={true}
+        disableDotsControls={true}
+        disableButtonsControls={true}
+        autoPlay={true}
+        autoPlayDirection="ltr"
+        autoPlayInterval={3000}
+        items={items}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
